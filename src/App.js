f9efbd01c5dcea24534846b70b7494ac97e6f889
anaele nnaemeka <anaelennaemeka@gmail.com>
2022-12-07 10:35:46 +0100

    graphQL Fetch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import DisplayLocations from "./components/DisplayLocations";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My first Apollo app 🚀</h2>
+      <DisplayLocations />
+    </div>
+  );
+}
+
+export default App;
