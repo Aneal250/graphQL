820bc4f3db8d7335de066e4dbb2bdda52b4cd1de
anaele nnaemeka <anaelennaemeka@gmail.com>
2022-12-08 19:12:20 +0100

    queryData

---
+++ src/components/GetDogs.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_DOG_PHOTO = gql`
+  query Dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+const GetDogs = ({ breed }) => {
+  const { loading, error, data } = useQuery(GET_DOG_PHOTO, {
+    variables: { breed },
+  });
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+
+  return (
+    <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+  );
+};
+
+export default GetDogs;
--- src/index.js
+++ src/index.js
@@ -3,39 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: "https://flyby-gateway.herokuapp.com/",
-  cache: new InMemoryCache(),
-});
 
-client
-  .query({
-    query: gql`
-      query GetLocations {
-        locations {
-          id
-          name
-          description
-          photo
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>
 );
 
